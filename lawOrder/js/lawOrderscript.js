b6718d566c1b404c497f2ebbd463d5dc853665de
sanjay-07_singh <sanjaysinghbisht114@gmail.com>
2019-06-02 22:47:10 +0530

    Some part of Calgary done

---
+++ lawOrder/js/lawOrderscript.js
@@ -0,0 +1,62 @@
+$(()=>{
+    $(".b").mouseover(()=>{
+        $(".b").css({"background-color":"transparent"});
+    });
+     $(".b").mouseout(()=>{
+        $(".b").css({"background-color":"#c00101"});
+    });
+    $(".k").mouseover(()=>{
+        $(".k").css({"background-color":"#c00101"});
+    });
+     $(".k").mouseout(()=>{
+        $(".k").css({"background-color":"transparent"});
+    });
+    $(".kb").mouseover(()=>{
+        $(".kb").css({"background-color":"white",
+                     "color":"#c00101"});
+    });
+     $(".kb").mouseout(()=>{
+        $(".kb").css({"background-color":"#c00101",
+                     "color":"white"});
+    });
+    $(".nl1").mouseover(()=>{
+        $(".nl1").css("color","#c00101");
+    });
+     $(".nl1").mouseout(()=>{
+        $(".nl1").css("color","white");
+    });
+     $(".nl2").mouseover(()=>{
+        $(".nl2").css("color","#c00101");
+    });
+     $(".nl2").mouseout(()=>{
+        $(".nl2").css("color","white");
+    });
+     $(".nl3").mouseover(()=>{
+        $(".nl3").css("color","#c00101");
+    });
+     $(".nl3").mouseout(()=>{
+        $(".nl3").css("color","white");
+    });
+     $(".nl4").mouseover(()=>{
+        $(".nl4").css("color","#c00101");
+    });
+     $(".nl4").mouseout(()=>{
+        $(".nl4").css("color","white");
+    });
+     $(".nl5").mouseover(()=>{
+        $(".nl5").css("color","#c00101");
+    });
+     $(".nl5").mouseout(()=>{
+        $(".nl5").css("color","white");
+    });
+     $(".nl6").mouseover(()=>{
+        $(".nl6").css("color","#c00101");
+    });
+     $(".nl6").mouseout(()=>{
+        $(".nl6").css("color","white");
+    });
+})
+
+
+
+
