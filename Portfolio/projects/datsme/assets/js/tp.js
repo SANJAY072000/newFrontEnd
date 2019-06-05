056949301d4bca4b048f3552b75fc71a9e723f37
sanjay-07_singh <sanjaysinghbisht114@gmail.com>
2019-06-05 16:20:38 +0530

    Portfolio Updated

---
+++ Portfolio/projects/datsme/assets/js/tp.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.blink', {
+  strings: ["<i>CONNECTED</i>^2000","<i>WHOLESOME</i>^2000","<i>AMAZING</i>^2000"],
+  typeSpeed: 100,
+    backSpeed: 100,
+    smartBackspace: true, 
+    cursorChar: '|',
+    loop: true
+});
